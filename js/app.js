1b5fc247f627a7d3c9a156ca8d9f8d6c2728dc54
masumbillaah <hhmasum06@gmail.com>
2021-09-13 20:32:12 +0600

    finally done my site

--- js/app.js
+++ js/app.js
@@ -13,30 +13,36 @@ const showProducts = (products) => {
     const div = document.createElement("div");
     div.classList.add("product");
     div.innerHTML = `<div class="single-product">
-      <div>
-    <img class="product-image" src=${image}></img>
-      </div>
+    <div>
+    <img class="product-image" src=${product.image}></img>
+    </div>
       <h3>${product.title}</h3>
       <p>Category: ${product.category}</p>
       <h2>Price: $ ${product.price}</h2>
       <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
-      <button id="details-btn" class="btn btn-danger">Details</button></div>
-      `;
+      <button id="details-btn" class="btn btn-danger">Details</button>
+      <h4>Rating: ${product.rating.rate} Count: ${product.rating.count}</h4>
+    </div>`;
     document.getElementById("all-products").appendChild(div);
   }
+
 };
+
 let count = 0;
 const addToCart = (id, price) => {
   count = count + 1;
   updatePrice("price", price);
 
   updateTaxAndCharge();
+
+  updateTotal();
   document.getElementById("total-Products").innerText = count;
 };
 
 const getInputValue = (id) => {
   const element = document.getElementById(id).innerText;
-  const converted = parseInt(element);
+  const converted = parseFloat(element);
+  console.log(typeof(converted));
   return converted;
 };
 
@@ -45,14 +51,15 @@ const updatePrice = (id, value) => {
   const convertedOldPrice = getInputValue(id);
   const convertPrice = parseFloat(value);
   const total = convertedOldPrice + convertPrice;
-  document.getElementById(id).innerText = Math.round(total);
+  document.getElementById(id).innerText = total.toFixed(2);
 };
 
 // set innerText function
 const setInnerText = (id, value) => {
-  document.getElementById(id).innerText = Math.round(value);
+  document.getElementById(id).innerText = parseFloat(value).toFixed(2);
 };
 
+// const priceConverted = getInputValue("price");
 // update delivery charge and total Tax
 const updateTaxAndCharge = () => {
   const priceConverted = getInputValue("price");
@@ -75,6 +82,12 @@ const updateTotal = () => {
   const grandTotal =
     getInputValue("price") + getInputValue("delivery-charge") +
     getInputValue("total-tax");
-  document.getElementById("total").innerText = grandTotal;
+  document.getElementById("total").innerText = parseFloat(grandTotal).toFixed(2);
 };
+
+// buy now button click (order confirmation)
+const placeOrder = () =>{
+  alert("Your order placed. Thank you.")
+}
 loadProducts();
+
